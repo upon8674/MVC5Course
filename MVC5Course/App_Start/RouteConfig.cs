9cf30a3e2d9aec26cc4d4e66335bf7aad618a419
upon <upon8674@gmail.com>
2017-05-06 11:20:30 +0800

    新增hello頁面 加入FabricsDB

--- MVC5Course/App_Start/RouteConfig.cs
+++ MVC5Course/App_Start/RouteConfig.cs
@@ -15,8 +15,12 @@ namespace MVC5Course
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{action}/{controller}/{id}",
+                defaults: new {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
             );
         }
     }
